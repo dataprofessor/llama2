ba4fe98249d2ecabc7d88c92d9c8c3f54aefef82
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-19 17:44:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,53 @@
 import streamlit as st
+import replicate
 
-st.title('🎈 App Name')
+# App title
+st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
-st.write('Hello world!')
+# Replicate Credentials
+with st.sidebar:
+    st.title('🦙💬 Llama 2 Chatbot')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API key:', type='password')
+        if not replicate_api:
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    st.markdown('📖 Learn how to build this app in this [blog](#link-to-blog)!')
+    
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# Function for generating LLM response
+def generate_response(prompt_input, api_token):
+    llm='a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+    pre_prompt='You are a helpful assistant.'
+    output=replicate.run(llm, 
+                         input={"prompt": pre_prompt + 'User: ' + prompt_input + 'Assistant: '}, 
+                         api_token=api_token)
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    #with st.chat_message("assistant"):
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_response(prompt, replicate_api) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
