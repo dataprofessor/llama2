d6db48e5f3478dec04a5a90d55d0f7f4eb918db9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-19 19:12:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,5 +56,5 @@ if st.session_state.messages[-1]["role"] != "assistant":
                 full_response += item
                 #st.markdown(full_response + "|")
             st.markdown(full_response)
-    message = {"role": "assistant", "content": response}
+    message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
