fdd7c710b83fcda8248b256934c98be8bb61c563
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-20 08:54:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,7 @@ for message in st.session_state.messages:
 # Function for generating LLaMA2 response
 # Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
-    pre_prompt = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
-    string_dialogue = pre_prompt
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
@@ -41,7 +40,11 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
 
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: "})
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                 "temperature": 0.1,
+                                 "top_p": 0.9,
+                                 "max_length": 512,
+                                 "repetition_penalty": 1})
     return output
 
 # User-provided prompt
