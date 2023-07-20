59ed19ec9dbcafeb94e23f4d1a0a3fed46f745fb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-20 23:20:39 +0700

    Update streamlit_app_v2.py

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -17,9 +17,9 @@ with st.sidebar:
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
-    #temperature = 
-    #top_p = 
-    #max_length = 
+    temperature = st.sidebar.slider('temperature:', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+    top_p = st.sidebar.slider('top_p:', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.sidebar.slider('max_length:', min_value=64, max_value=4096, value=2048, step=8)
     st.markdown('📖 Learn how to build this app in this [blog](#link-to-blog)!')
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
