ae47ca6a5b062f2b8a16fcf24e9e14fb557b7b96
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-20 09:02:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,13 +38,9 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                 "temperature": 0.1,
-                                 "top_p": 0.9,
-                                 "max_length": 512,
-                                 "repetition_penalty": 1})
+                                  "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
     return output
 
 # User-provided prompt
